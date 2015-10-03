b11a2bd04a07ef4c4bc4000fdace0ed9ebb4af3d
Matías Cubero <matias.cubero@moove-it.com>
2015-10-02 22:59:21 -0300

    Add database support for alias

--- lib/alias.rb
+++ lib/alias.rb
@@ -1,6 +1,11 @@
 module Alias
   def slack_name_for(user)
-    @@aliases ||= YAML.load(File.read('config/aliases.yml'))['aliases']
-    @@aliases[user] || user
+    if Application.instance.use_database?
+      user_mapping = UserMapping.where(gerrit_name: user).first
+      user_mapping ? user_mapping.slack_name : user
+    else
+      @@aliases ||= YAML.load(File.read('config/aliases.yml'))['aliases']
+      @@aliases[user] || user
+    end
   end
 end
